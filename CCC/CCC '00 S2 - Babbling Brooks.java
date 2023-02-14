690f8c992ab3fea6f678714db336b3bf6a7e882a
Simar Ubhi <73083235+simarubhi@users.noreply.github.com>
2023-02-14 11:24:46 -0600

    Create CCC '00 S2 - Babbling Brooks.java

---
+++ CCC/CCC '00 S2 - Babbling Brooks.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner r = new Scanner(System.in);
+	
+		int n = r.nextInt();
+		
+		ArrayList<Double> rivers = new ArrayList<Double>();
+		
+		for (int i = 0; i < n; i++) {
+			
+			rivers.add(r.nextDouble());
+		}
+
+		
+		int in;
+
+		do {
+			
+			in = r.nextInt();
+			if (in == 77) break;
+			
+			if (in == 99) {
+				int index = r.nextInt();
+				int leftFlow = r.nextInt();
+				
+				rivers.add(index - 1, rivers.get(index - 1) * leftFlow / 100);
+				rivers.set(index, rivers.get(index) * (100 - leftFlow) / 100);
+
+				
+			} else if (in == 88) {
+				int index = r.nextInt();				
+
+				rivers.set(index - 1, rivers.get(index - 1) + rivers.get(index));
+				rivers.remove(index);
+				
+			}
+			
+		} while (in != 77);
+		
+		for (int i = 0; i < rivers.size(); i++) {
+			System.out.print((int)Math.round(rivers.get(i)) + " ");
+		}
+
+		
+		
+		r.close();
+	}
+	
+}
